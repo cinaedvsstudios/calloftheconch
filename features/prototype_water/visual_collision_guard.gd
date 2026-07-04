9cdc9d82ea0373efb37842a04388eda1bea102cd
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-05 00:58:03 +0200

    Disable current lower overlay draw

--- features/prototype_water/visual_collision_guard.gd
+++ features/prototype_water/visual_collision_guard.gd
@@ -2,4 +2,15 @@ class_name VisualCollisionGuard
 extends Node
 
 func _ready() -> void:
-	set_process(false)
+	call_deferred("_disable_lower_overlay")
+
+func _disable_lower_overlay() -> void:
+	var parent_node: Node = get_parent()
+	if parent_node == null:
+		return
+	var water_layer: Node = parent_node.get_node_or_null("WaterLayer")
+	if water_layer == null:
+		return
+	for child: Node in water_layer.get_children():
+		if child is Sprite2D and child.z_index == -20:
+			child.visible = false
