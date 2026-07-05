a534810939873400f4419cfdaa6e37dc8ce95651
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-05 04:18:35 +0200

    Add V2 looping audio player behaviour

---
+++ rebuild_v2/shared/looping_audio_player_v2.gd
@@ -0,0 +1,14 @@
+class_name LoopingAudioPlayerV2
+extends AudioStreamPlayer
+
+@export var loop_stream: bool = true
+
+func _ready() -> void:
+	if loop_stream:
+		_set_stream_looping()
+
+func _set_stream_looping() -> void:
+	if stream is AudioStreamMP3:
+		(stream as AudioStreamMP3).loop = true
+	elif stream is AudioStreamOggVorbis:
+		(stream as AudioStreamOggVorbis).loop = true
