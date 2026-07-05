76662597fac0247f39ce8c5bdbacf59f6c4604a6
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-05 02:22:09 +0200

    Toggle V2 gameplay scene visibility

--- rebuild_v2/app/contexts/gameplay_v2/gameplay_context_v2.gd
+++ rebuild_v2/app/contexts/gameplay_v2/gameplay_context_v2.gd
@@ -5,11 +5,14 @@ signal menu_requested
 
 @onready var _level: SeaOfPillarsV2 = %SeaOfPillarsV2
 @onready var _gameplay_music: AudioStreamPlayer = %GameplayMusic
+@onready var _gameplay_ui: CanvasLayer = $GameplayUI
 
 func activate() -> void:
+	_gameplay_ui.visible = true
 	_level.activate()
 	_gameplay_music.play()
 
 func deactivate() -> void:
+	_gameplay_ui.visible = false
 	_gameplay_music.stop()
 	_level.deactivate()
