f14dc524edd00ef040290e56a3445a1f9a166803
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-05 02:22:55 +0200

    Connect V2 pause overlay to gameplay context

--- rebuild_v2/app/contexts/gameplay_v2/gameplay_context_v2.gd
+++ rebuild_v2/app/contexts/gameplay_v2/gameplay_context_v2.gd
@@ -6,6 +6,10 @@ signal menu_requested
 @onready var _level: SeaOfPillarsV2 = %SeaOfPillarsV2
 @onready var _gameplay_music: AudioStreamPlayer = %GameplayMusic
 @onready var _gameplay_ui: CanvasLayer = $GameplayUI
+@onready var _pause_overlay: PauseOverlayV2 = %PauseOverlay
+
+func _ready() -> void:
+	_pause_overlay.menu_requested.connect(_on_pause_menu_requested)
 
 func activate() -> void:
 	_gameplay_ui.visible = true
@@ -16,3 +20,6 @@ func deactivate() -> void:
 	_gameplay_ui.visible = false
 	_gameplay_music.stop()
 	_level.deactivate()
+
+func _on_pause_menu_requested() -> void:
+	menu_requested.emit()
