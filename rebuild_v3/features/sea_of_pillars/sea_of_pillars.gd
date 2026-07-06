ee6f35ff55c6222e1ea27df384a5bf88e599d81b
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-06 16:30:44 +0200

    Disable full-screen ambient bubble overlay by default

--- rebuild_v3/features/sea_of_pillars/sea_of_pillars.gd
+++ rebuild_v3/features/sea_of_pillars/sea_of_pillars.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 @export_category("Level")
 @export var auto_play_ambience: bool = true
-@export var auto_play_bubble_overlay: bool = true
+@export var auto_play_bubble_overlay: bool = false
 
 @onready var _hylas: CotcHylas = %Hylas
 @onready var _start_marker: Marker2D = %HylasStart
