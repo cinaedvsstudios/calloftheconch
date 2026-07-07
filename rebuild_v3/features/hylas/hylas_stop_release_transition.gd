72995affa2fd7c3e353438e49e3b284fc6ce436d
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-08 01:11:49 +0200

    Add Hylas stop release transition behavior

---
+++ rebuild_v3/features/hylas/hylas_stop_release_transition.gd
@@ -0,0 +1,35 @@
+extends AnimatedSprite2D
+
+## Displays the one-frame stop release pose before Hylas returns to idle.
+
+const STOP_RELEASE_DURATION: float = 0.12
+
+var _shift_was_held: bool = false
+var _stop_was_active: bool = false
+var _release_remaining: float = 0.0
+
+
+func _process(delta: float) -> void:
+	var shift_is_held: bool = Input.is_action_pressed(&"action_a")
+
+	if animation == &"stop" and shift_is_held:
+		_stop_was_active = true
+
+	if _shift_was_held and not shift_is_held and _stop_was_active:
+		_stop_was_active = false
+		_release_remaining = STOP_RELEASE_DURATION
+		_play_release_frame()
+
+	if _release_remaining > 0.0:
+		_release_remaining = maxf(0.0, _release_remaining - delta)
+		_play_release_frame()
+		if _release_remaining <= 0.0:
+			play(&"idle")
+
+	_shift_was_held = shift_is_held
+
+
+func _play_release_frame() -> void:
+	animation = &"stop_release"
+	frame = 0
+	pause()
