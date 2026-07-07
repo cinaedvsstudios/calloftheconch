dbb8f7d61522a11d9d7e1fb62e9d5578df6509ea
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-08 01:58:20 +0200

    Use gravity for Hylas land jumps and falls

--- rebuild_v3/features/hylas/hylas_land_controller.gd
+++ rebuild_v3/features/hylas/hylas_land_controller.gd
@@ -1,45 +1,69 @@
 extends "res://rebuild_v3/features/hylas/hylas_motion_presentation.gd"
 
+@export_category("Land Movement")
+@export var land_gravity := 5800.0
+@export var jump_horizontal_speed := 720.0
+@export var jump_launch_upward_speed := 1665.0
 @export var crawl_speed := 170.0
 @export var crawl_water_reentry_depth := 36.0
+
 @onready var splash: AudioStreamPlayer = %SplashAudio
+@onready var hurt_audio: AudioStreamPlayer = %HurtAudio
+@onready var land_impact_audio: AudioStreamPlayer = %LandImpactAudio
+
 var crawl_active := false
+var airborne_active := false
+var left_water := false
 var entry_splash_done := false
+
+
+func _ready() -> void:
+	super._ready()
+	splash.process_mode = Node.PROCESS_MODE_ALWAYS
+	hurt_audio.process_mode = Node.PROCESS_MODE_ALWAYS
+	land_impact_audio.process_mode = Node.PROCESS_MODE_ALWAYS
+
 
 func _physics_process(delta: float) -> void:
 	if crawl_active:
 		crawl_update(delta)
 		return
+	if airborne_active:
+		airborne_update(delta)
+		return
 	super._physics_process(delta)
+
 
 func _begin_surface_jump() -> void:
 	crawl_active = false
+	airborne_active = true
+	left_water = false
 	entry_splash_done = false
 	super._begin_surface_jump()
+	var facing_sign := -1.0 if _facing_left else 1.0
+	velocity = Vector2(facing_sign * jump_horizontal_speed, -jump_launch_upward_speed)
 	play_splash()
 
-func _update_surface_jump(delta: float) -> void:
+
+func airborne_update(delta: float) -> void:
+	if not _play_enabled:
+		return
 	_jump_elapsed += delta
-	var p := clampf(_jump_elapsed / maxf(0.01, jump_duration), 0.0, 1.0)
-	var b := _jump_start.lerp(_jump_end, p)
-	var next := Vector2(b.x, b.y - sin(p * PI) * jump_arc_height)
-	var above := global_position.y < _swim_ceiling_y
-	if next.y > global_position.y:
-		if move_and_collide(next - global_position) != null:
-			begin_crawl()
-			return
-	else:
-		global_position = next
-	if not entry_splash_done and above and global_position.y >= _swim_ceiling_y:
-		entry_splash_done = true
-		surface_splash_requested.emit(Vector2(global_position.x, _swim_ceiling_y), false)
-		play_splash()
-	if p >= 1.0:
-		_jump_elapsed = 0.0
-		global_position = _clamp_to_world(_jump_end)
-		_set_animation(&"idle")
+	velocity.y += land_gravity * delta
+	move_and_slide()
+	global_position = clamp_airborne_position(global_position)
+
+	if global_position.y < _swim_ceiling_y:
+		left_water = true
+	if is_on_floor():
+		begin_crawl()
+		return
+	if left_water and velocity.y > 0.0 and global_position.y >= _swim_ceiling_y:
+		enter_water()
+
 
 func begin_crawl() -> void:
+	airborne_active = false
 	crawl_active = true
 	_jump_elapsed = 0.0
 	_pending_surface_jump = false
@@ -48,9 +72,24 @@ func begin_crawl() -> void:
 	_special_velocity = Vector2.ZERO
 	velocity = Vector2.ZERO
 	_set_visual_rotation(0.0)
-	global_position = _clamp_to_world(global_position)
 	_set_animation(&"crawl")
 	_stop_movement_audio()
+	play_hurt_sound()
+	play_land_impact_sound()
+
+
+func enter_water() -> void:
+	airborne_active = false
+	_jump_elapsed = 0.0
+	velocity = Vector2.ZERO
+	global_position.y = _swim_ceiling_y + crawl_water_reentry_depth
+	if not entry_splash_done:
+		entry_splash_done = true
+		surface_splash_requested.emit(Vector2(global_position.x, _swim_ceiling_y), false)
+		play_splash()
+	_set_visual_rotation(0.0)
+	_set_animation(&"idle")
+
 
 func crawl_update(delta: float) -> void:
 	if not _play_enabled:
@@ -60,32 +99,56 @@ func crawl_update(delta: float) -> void:
 	_update_camera_shake(delta)
 	_update_shadow()
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
-	if absf(direction.x) > 0.01:
-		_facing_left = direction.x < 0.0
+	var floor_normal := get_floor_normal()
+	var ground_tangent := Vector2(-floor_normal.y, floor_normal.x)
+	var crawl_axis := direction.dot(ground_tangent)
+	if absf(crawl_axis) > 0.01:
+		_facing_left = crawl_axis < 0.0
+	velocity = ground_tangent * crawl_axis * crawl_speed
+	velocity.y += land_gravity * delta
 	_set_visual_rotation(0.0)
 	_stop_movement_audio()
 	_set_animation(&"crawl")
-	if direction == Vector2.ZERO:
-		velocity = Vector2.ZERO
+	if absf(crawl_axis) <= 0.01:
 		_animated_sprite.frame = 0
 		_animated_sprite.pause()
 	else:
-		velocity = direction.normalized() * crawl_speed
 		_animated_sprite.play()
-		move_and_slide()
-		global_position = _clamp_to_world(global_position)
+	move_and_slide()
+	global_position = _clamp_to_world(global_position)
+	if not is_on_floor():
+		crawl_active = false
+		airborne_active = true
+		_set_animation(&"jump")
+		_animated_sprite.frame = _animated_sprite.sprite_frames.get_frame_count(&"jump") - 1
+		_animated_sprite.pause()
+		return
 	if global_position.y >= _swim_ceiling_y + crawl_water_reentry_depth:
 		crawl_active = false
 		velocity = Vector2.ZERO
 		_set_animation(&"idle")
 
-func _clamp_to_world(value: Vector2) -> Vector2:
-	var min_y := _world_bounds.position.y + player_edge_padding
-	if _jump_elapsed <= 0.0 and not crawl_active:
-		min_y = maxf(min_y, _swim_ceiling_y)
-	return Vector2(clampf(value.x, _world_bounds.position.x + player_edge_padding, _world_bounds.end.x - player_edge_padding), clampf(value.y, min_y, _world_bounds.end.y - player_edge_padding))
+
+func clamp_airborne_position(value: Vector2) -> Vector2:
+	return Vector2(
+		clampf(value.x, _world_bounds.position.x + player_edge_padding, _world_bounds.end.x - player_edge_padding),
+		clampf(value.y, _world_bounds.position.y + player_edge_padding, _world_bounds.end.y - player_edge_padding),
+	)
+
 
 func play_splash() -> void:
 	if splash.stream != null:
 		splash.stop()
 		splash.play()
+
+
+func play_hurt_sound() -> void:
+	if hurt_audio.stream != null:
+		hurt_audio.stop()
+		hurt_audio.play()
+
+
+func play_land_impact_sound() -> void:
+	if land_impact_audio.stream != null:
+		land_impact_audio.stop()
+		land_impact_audio.play()
