d65e635ed2977dd3453a325e93d6b5f60d086450
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-11 00:05:25 +0200

    Add Hylas action collision profiles

--- rebuild_v3/features/hylas/hylas_motion_presentation.gd
+++ rebuild_v3/features/hylas/hylas_motion_presentation.gd
@@ -10,19 +10,54 @@ const BURST_HOLD_END: float = 1.20
 const BURST_FINISH_FRAME_DURATION: float = 0.10
 const IDLE_ANIMATION_FPS: float = 2.50
 
+@export_category("Collision Profiles")
+@export var default_collision_shape: Shape2D
+@export var tail_flip_collision_shape: Shape2D
+@export var conch_collision_shape: Shape2D
+@export var stop_collision_shape: Shape2D
+
 var _stop_pose_held: bool = false
+var _collision_profile_name: StringName = &"default"
 
 
 func _ready() -> void:
 	burst_max_duration = BURST_DURATION
 	super._ready()
 	_animated_sprite.sprite_frames.set_animation_speed(&"idle", IDLE_ANIMATION_FPS)
+	_apply_collision_profile(_animated_sprite.animation)
 
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
 	_update_stop_pose_hold()
 	_update_burst_presentation()
+	_apply_collision_profile(_animated_sprite.animation)
+
+
+func _set_animation(animation_name: StringName) -> void:
+	super._set_animation(animation_name)
+	_apply_collision_profile(animation_name)
+
+
+func _apply_collision_profile(animation_name: StringName) -> void:
+	var target_shape: Shape2D = default_collision_shape
+	var target_profile: StringName = &"default"
+
+	if animation_name == &"tail_flip":
+		target_shape = tail_flip_collision_shape
+		target_profile = &"tail_flip"
+	elif animation_name == &"conch":
+		target_shape = conch_collision_shape
+		target_profile = &"conch"
+	elif animation_name == &"stop" or animation_name == &"stop_release":
+		target_shape = stop_collision_shape
+		target_profile = &"stop"
+
+	if target_shape == null:
+		return
+	if _collision_shape.shape != target_shape:
+		_collision_shape.shape = target_shape
+	_collision_profile_name = target_profile
 
 
 func _update_stop_pose_hold() -> void:
