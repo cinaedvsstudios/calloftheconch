f76d2a18f5a03f07c9b93056f83d716ee5193c19
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-13 17:12:27 +0200

    Add Settings access to the in-game pause menu

--- rebuild_v3/app/contexts/gameplay/pause_overlay.gd
+++ rebuild_v3/app/contexts/gameplay/pause_overlay.gd
@@ -1,23 +1,35 @@
 class_name CotcPauseOverlay
 extends Control
 
+signal settings_requested
 signal menu_requested
 
 @onready var _resume_button: Button = %ResumeButton
+@onready var _settings_button: Button = %SettingsButton
 @onready var _menu_button: Button = %MenuButton
+
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	_resume_button.pressed.connect(close_overlay)
+	_settings_button.pressed.connect(_open_settings)
 	_menu_button.pressed.connect(_menu)
 	hide()
+
 
 func open_overlay() -> void:
 	show()
 	_resume_button.grab_focus()
 
+
 func close_overlay() -> void:
 	hide()
+
+
+func _open_settings() -> void:
+	hide()
+	settings_requested.emit()
+
 
 func _menu() -> void:
 	hide()
