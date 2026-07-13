176099cb2c465c03f9e7c083dc4fda1902272e45
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-13 22:56:59 +0200

    Resume gameplay from pause button

--- rebuild_v3/app/contexts/gameplay/gameplay_context.gd
+++ rebuild_v3/app/contexts/gameplay/gameplay_context.gd
@@ -26,6 +26,7 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	_gameplay_music.process_mode = Node.PROCESS_MODE_ALWAYS
 	_hint.hide()
+	_pause_overlay.resume_requested.connect(_on_pause_resume_requested)
 	_pause_overlay.settings_requested.connect(_on_pause_settings_requested)
 	_pause_overlay.menu_requested.connect(_on_pause_menu_requested)
 	_death_overlay.continue_requested.connect(_on_death_continue_requested)
@@ -131,6 +132,12 @@ func _unhandled_input(event: InputEvent) -> void:
 			get_tree().paused = true
 			_pause_overlay.open_overlay()
 		get_viewport().set_input_as_handled()
+
+
+func _on_pause_resume_requested() -> void:
+	if not _active:
+		return
+	get_tree().paused = false
 
 
 func _on_pause_settings_requested() -> void:
