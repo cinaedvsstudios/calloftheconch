f6a45af6f6b793e313e3eb0cacbcaacb5e83cf0a
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-20 05:28:08 +0200

    Use configured vacuum output launch speed

--- rebuild_v3/features/vacuum_vent/vacuum_vent_output.gd
+++ rebuild_v3/features/vacuum_vent/vacuum_vent_output.gd
@@ -6,7 +6,7 @@ extends "res://rebuild_v3/features/vent1/vent1.gd"
 
 @export_category("Linked Ejection")
 @export_range(100.0, 2400.0, 10.0) var transport_launch_speed: float = 980.0
-@export_range(20.0, 500.0, 5.0) var emergence_distance: float = 185.0
+@export_range(20.0, 500.0, 5.0) var minimum_emergence_distance: float = 185.0
 
 @onready var _exit_marker: Marker2D = %ExitMarker
 
@@ -24,14 +24,18 @@ func get_transport_launch_velocity() -> Vector2:
 	return get_launch_direction() * transport_launch_speed
 
 
-func get_emergence_target_global_position() -> Vector2:
-	return get_exit_global_position() + get_launch_direction() * emergence_distance
+func get_emergence_target_global_position(emergence_seconds: float) -> Vector2:
+	var launch_distance: float = maxf(
+		minimum_emergence_distance,
+		transport_launch_speed * maxf(0.0, emergence_seconds),
+	)
+	return get_exit_global_position() + get_launch_direction() * launch_distance
 
 
 func get_debug_lines() -> Array[String]:
 	var direction: Vector2 = get_launch_direction()
 	return [
 		"vacuum_output_launch_speed=%.1f" % transport_launch_speed,
-		"vacuum_output_emergence_distance=%.1f" % emergence_distance,
+		"vacuum_output_minimum_distance=%.1f" % minimum_emergence_distance,
 		"vacuum_output_direction=(%.3f, %.3f)" % [direction.x, direction.y],
 	]
